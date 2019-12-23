14f45a5274208fa756898d2b13094984b0647fda
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2019-12-23 13:23:15 -0500

    Update main.code.R

--- main.code.R
+++ main.code.R
@@ -50,3 +50,125 @@ areasm<-pipeit(main_links[10],"td:nth-child(2)")[-c(288:292)]
 Ontario_ds<-data.frame("Area Code"=c(unlist(pcd),pcdm),
                        "Locale"=c(unlist(areas),areasm))
 Ontario_ds<-Ontario_ds[order(Ontario_ds$Area.Code),]
+
+####lets get a sample data set using the yellowpages webcraper
+library(stringi)
+library(tibble)
+ypca_scrape<-function(url){
+  ###############Functions needed#############################
+  pipeit<-function(url,code){
+    read_html(url)%>%html_nodes(code)%>%html_text()
+  }
+  
+  pipelink<-function(url,code){
+    read_html(url)%>%html_nodes(code)%>%html_attr("href")
+  }
+  ###################Scraper 
+  scrape<-function(link){
+    
+    
+    
+    ##################################################
+    
+    
+    #################GET LINKS###################
+    
+    
+    ##main link###
+    
+    m<-"https://www.yellowpages.ca"
+    
+    ##extract sublinks##
+    
+    sb<-pipelink(link,".jsListingName")
+    
+    ##good sublinkslinks##
+    gl<-paste0(m,sb)
+    
+    
+    
+    ###################SCRAPING###################
+    
+    ##name##
+    nm<-sapply(gl,function(x){Sys.sleep(sample(10, 1) * 0.1) 
+      pipeit(x,".merchant__title")})
+    nm<-unname(nm)
+    nm<-sapply(nm,function(x) x[1])
+    nm<-nm[c(TRUE, FALSE)]
+    
+    ##Address##
+    ad<-sapply(gl,function(x) { Sys.sleep(sample(10, 1) * 0.1)
+      pipeit(x,".merchant__address__mobile")})
+    ad<-unname(ad)
+    ad<-gsub("\\n","",ad)
+    
+    ##Phone Number##
+    pn<-sapply(gl,function(x){Sys.sleep(sample(10, 1) * 0.1)
+      pipeit(x,".mlr__item--phone .mlr__submenu__item")})
+    pn<-unname(pn)
+    pn<-lapply(pn,function(x)unique(x))
+    pn<-lapply(pn,function(x) paste(x))
+    ##Website##
+    wb<-sapply(gl,function(x){Sys.sleep(sample(10, 1) * 0.1)
+      pipelink(x,".mlr__item__cta.hide-print")})
+    wb<-unname(wb)
+    wb<-suppressWarnings(str_extract(wb,"%3A%2F%2F(.*?)%2F"))
+    wb<-gsub("%3A%2F%2F","",wb)
+    wb<-gsub("%2F","",wb)
+    
+    ##Rating##
+    rt<-sapply(gl,function(x){Sys.sleep(sample(10, 1) * 0.1)
+      pipeit(x,".merchant__overall_rating")[1]})
+    ##Data frame##
+    dt<-tibble(nm,ad,pn,wb,rt)
+    dt
+    
+  }
+  
+  
+  ##number of pages##
+  n<-pipeit(url,".pageCount span:nth-child(2)")
+  n<-gsub("\\n","",n)
+  n<-as.numeric(n)
+  #Split and build url
+  spliturl<-strsplit(url,"1")
+  first<-spliturl[[1]][1]
+  last<-spliturl[[1]][2]
+  newurls<-c()
+  for(i in 1:n){
+    newurls[i]<-paste0(first,i,last)
+  }
+  
+  ###Scrape and shape data###
+  df<-sapply(newurls,function(x){Sys.sleep(sample(10, 1) * 0.1) 
+    scrape(x)})
+  ##Shape Data##
+  dt<-sapply(df,unname)
+  dt<-unname(df)
+  dt<-rbind(df)
+  nb<-lapply(dt[3,],function(x) paste(x))
+  nb<-lapply(nb,function(x) sapply(x,function(y) gsub('c\\("',"",y)))
+  nb<-lapply(nb,function(x) sapply(x,function(y) gsub('\"',"",y)))
+  nb<-lapply(nb,function(x) sapply(x,function(y) gsub('\\)',"",y)))
+  dt[3,]<-lapply(nb, unname)
+  
+  dt<-t(dt)
+  rownames(dt)<-NULL
+  
+  dt 
+}
+
+###Music  in Ontario
+dat<-ypca_scrape("https://www.yellowpages.ca/search/si/1/music+stores/Ontario+ON")
+
+d1<-unlist(dat[,1])
+d2<-unlist(dat[,2])
+d3<-unlist(dat[,3])
+d4<-unlist(dat[,4])
+d5<-unlist(dat[,5])
+
+dt<-tibble(d1,d2,d3,d4,d5)
+
+
+##
+library(plyr)
