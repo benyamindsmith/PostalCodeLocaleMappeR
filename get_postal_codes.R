af3003e5d69ea6f143ed716c4f6a8c6c0480b35c
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2019-12-25 13:14:31 -0500

    Update get_postal_codes.R

--- get_postal_codes.R
+++ get_postal_codes.R
@@ -1,7 +1,5 @@
-
-require(stringr)
-
 get_postal_codes<-function(x){
+  require(stringr)
   str_extract_all(x,
                   "[ABCEGHJKLMNPRSTVXY]\\d[ABCEGHJ-NPRSTV-Z][ ]?\\d[ABCEGHJ-NPRSTV-Z]\\d")
 }
